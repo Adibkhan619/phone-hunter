ef79e569a293c54edcee57a40d6842d6047cb064
Adibkhan619 <work.adibkhan@gmail.com>
2024-02-28 20:44:44 +0600

    second commit

--- phone.js
+++ phone.js
@@ -1,4 +1,4 @@
-const loadPhone = async (searchText) => {
+const loadPhone = async (searchText='13') => {
     const res = await fetch(`https://openapi.programming-hero.com/api/phones?search=${searchText}`);
     const data = await res.json();
     const phones = data.data;
@@ -15,17 +15,17 @@ const displayPhones = phones => {
 
     // display 'show all' button if there are more than 12 phones
     const showAllContainer = document.getElementById('show-all-container')
-    if(phones.length > 12){
+    if (phones.length > 12) {
         showAllContainer.classList.remove('hidden');
-    }else{
+    } else {
         showAllContainer.classList.add('hidden');
     }
 
     // display only 12 phone
-    phones = phones.slice(0,12);
+    phones = phones.slice(0, 12);
 
     phones.forEach(phone => {
-        console.log(phone);
+        // console.log(phone);
         // step-2: create a div
         const phoneCard = document.createElement('div');
         phoneCard.classList = `card p-8 bg-gray-200 shadow-xl`;
@@ -33,22 +33,25 @@ const displayPhones = phones => {
         // step-3: innerHTML
         phoneCard.innerHTML = `
         <figure><img src="${phone.image}" alt="Shoes" /></figure>
-                    <div class="card-body">
+                    <div class="card-body text-left text-black">
                       <h2 class="card-title">${phone.phone_name}</h2>
                       <p>If a dog chews shoes whose shoes does he choose?</p>
-                      <div class="card-actions justify-end">
-                        <button class="btn btn-primary">Buy Now</button>
+                      <div class="card-actions justify-start">
+                        <button onclick="handleShowDetails('${phone.slug}') " class="btn  btn-primary">Buy Now</button>
                       </div>
                 </div>`
         // step-4: append child
         phoneContainer.appendChild(phoneCard);
 
     })
+    // hide loading spinner
+    toggleLoadingSpinner(false);
 }
 
 // handle search button
 const handleSearch = () => {
-    const searchField =  document.getElementById('search-field');
+    toggleLoadingSpinner(true);
+    const searchField = document.getElementById('search-field');
     const searchText = searchField.value;
     console.log(searchText);
     loadPhone(searchText);
@@ -56,9 +59,57 @@ const handleSearch = () => {
 
 
 // handle search 2
-const handleSearch2 = () =>{
-    const searchField2 = document.getElementById('search-field-2');
-    const searchText = searchField2.value;
-    loadPhone(searchText);
+// const handleSearch2 = () =>{
+//     toggleLoadingSpinner(true);
+//     const searchField2 = document.getElementById('search-field-2');
+//     const searchText = searchField2.value;
+//     loadPhone(searchText);
+// }
+
+
+// loading spinner
+const toggleLoadingSpinner = (isLoading) => {
+    const loadingSpinner = document.getElementById('loading-spinner');
+    loadingSpinner.classList.remove('hidden');
+    if (isLoading) {
+        loadingSpinner.classList.remove('hidden');
+    } else {
+        loadingSpinner.classList.add('hidden');
+    }
 }
 
+// show details button
+const handleShowDetails = async(id) => {
+    // console.log('clicked', id);
+    // load single phone data
+    const res = await fetch(`https://openapi.programming-hero.com/api/phone/${id}`);
+    const data = await res.json();
+    const phone = data.data;
+
+    showPhoneDetails(phone);
+
+}
+// phone details on modal
+const showPhoneDetails = (phone) => {
+    console.log(phone);
+    const phoneName = document.getElementById('show-details-phone-name');
+    phoneName.innerText = phone.name;
+
+    const showDetailContainer = document.getElementById('show-details-container');
+
+    showDetailContainer.innerHTML = `
+    <img src="${phone.image}" />
+    <p><span>Storage: </span>${phone?.
+        mainFeatures?.storage}</p>
+    <p><span>Display Size: </span>${phone?.mainFeatures?.displaySize}</p>
+    <p><span>GPS: </span>${phone?.others?.GPS || 'No GPS'}</p>      
+    `
+
+
+    // show the modal
+    show_details_modal.showModal()
+}
+
+
+
+loadPhone();
